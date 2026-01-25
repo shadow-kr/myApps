bfb0e7e5be01890ac3f0bba76cc9aa8623074328
shadow-kr <81633366+shadow-kr@users.noreply.github.com>
2026-01-26 00:17:08 +0100

    Create cuting_images_multip_loading.py

---
+++ screen_cutting/cuting_images_multip_loading.py
@@ -0,0 +1,44 @@
+
+from PIL import Image
+import os
+from tqdm import tqdm
+
+"""
+Couper plusieurs images en deux depuis un dossier, utilisé pur les captures à deux ecran
+creer les dossier
+"""
+
+#source
+path_image          = "C:/Users/toshiba/Pictures/Screenshots/Sceenshots 25-26/"
+
+#destination
+path_ma_capture     = "C:/Users/toshiba/Pictures/Screenshots/Screen voulu/"
+path_non_voulu      = "C:/Users/toshiba/Pictures/Screenshots/Screen non voulu/"
+
+
+def cuting(file_path,filename):
+    image = Image.open(file_path)
+
+    width, height = image.size
+
+    # print("width : ", width)
+    # print("height : ", height)
+
+    left_image = image.crop((0, 0, width // 2, height))
+    right_image = image.crop((width // 2, 0, width, height))
+
+    # ici on sauvgarde les deux partis de l'image
+    left_image.save(path_non_voulu + filename)
+    right_image.save(path_ma_capture + filename)    
+
+
+# si je veux pas tqdm --> for filename in os.listdir(path_image):
+for filename in tqdm(os.listdir(path_image), desc="Processing Images", unit="image"):
+
+    image_to_cut = os.path.join(path_image, filename)
+    
+    # print(image_to_cut)
+    cuting(image_to_cut,filename)
+        
+
+print("Done and done")
